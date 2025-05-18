0e11f1411ea6d17bf336258261bdcdc593409d72
MEAL0015 <merjam.al-beibani0015@stud.hkr.se>
2025-05-18 22:47:51 +0200

    render issues

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 // Route imports
 const authRoutes = require('./routes/auth');
