9c983a7729102c3a3a1476f75116daa62e93fd6c
MEAL0015 <merjam.al-beibani0015@stud.hkr.se>
2025-05-18 16:34:55 +0200

    added some missing stuffs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/home'; //case sensitive here
-import Register from './pages/Register.jsx'; //case sensitive it should be capital but tried this
+import Home from './pages/home.jsx';
+import Register from './pages/Register.jsx';
 import About from './pages/About.jsx';
 import Login from './pages/Login.jsx';
 import Admin from './pages/Admin.jsx';
@@ -16,8 +16,8 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home.jsx />} />
+        <Route path="/" element={<Navigate to="/Home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
