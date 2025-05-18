87694910b38a4ec3725108ae20a4450616324337
MEAL0015 <merjam.al-beibani0015@stud.hkr.se>
2025-05-18 22:51:06 +0200

    fixing so render works

--- server/index.js
+++ server/index.js
@@ -3,13 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-}
 
 // Route imports
 const authRoutes = require('./routes/auth');
@@ -23,6 +16,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
+
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -39,10 +41,6 @@ app.use('/api/activities', activityRoutes); // New route for activities
 app.use('/api/contact', contactRoutes);
 
 
-app.get('/', (req, res) => {
-  res.send('UniVibe backend is running 🚀');
-
-  });
 // Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
