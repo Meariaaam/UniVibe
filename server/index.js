ffff1ef2d68d870167ed9e817aa6eb1e43cf35df
MEAL0015 <merjam.al-beibani0015@stud.hkr.se>
2025-05-19 11:03:55 +0200

    new things

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const contactRoutes = require('./routes/contact');
 
 const app = express();
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
